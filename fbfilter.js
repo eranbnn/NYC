aeafa87040137e91cdfee5bcc0fd17a2eb884943
Eran Bentziony <eran@500tech.com>
2017-02-04 14:36:50 +0200

    separate code into two files.

---
+++ fbfilter.js
@@ -0,0 +1,43 @@
+
+var pageID;
+var nextPage;
+
+function getPosts(form) {
+	pageID = form[0].value;
+	FB.api(
+		"/" + pageID + "/feed",
+		{limit: 100},
+		handleRespone
+	);
+	event.preventDefault();
+}
+
+function getMore() {
+	FB.api(
+		"/",
+		{id: nextPage.replace(/\//g,"\\/")},
+		handleRespone
+	);
+}
+
+function createUrl(commentID) {
+	return "https://www.facebook.com/" + pageID + "/posts/" + commentID.split("_")[0] + "?comment_id=" + commentID.split("_")[1];
+}
+	
+function handleRespone(response) {
+	response.data.forEach(function (post) {
+		FB.api(
+			"/" + post.id + "/comments",
+			function (response) {
+			  if (response && !response.error) {
+				for (var i = 0 ; i < response.data.length ; i++) {
+					if (response.data[i].from.id === pageID) {
+						document.body.innerHTML += "<a href='" + createUrl(response.data[i].id) + "' target='_blank'>" + response.data[i].message + "</a>" + "<br/><br/>";
+					}
+				}
+			  }
+			}
+		);
+	});
+	nextPage = response.paging.next;
+}
